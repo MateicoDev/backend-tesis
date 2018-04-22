43bc067212911799f3a2d358100d03212fb5e58a
Juan Diaz <juan.diaz@dinocloudconsulting.com>
2018-04-22 13:11:12 -0300

    Populate the status and types of claims

--- populators/claim_initializer.py
+++ populators/claim_initializer.py
@@ -5,16 +5,16 @@ from database import db
 class ClaimInitializer():
     def init_status_claims(self):
         rows = []
-        if not ClaimStatus.query.filter(ClaimStatus.name =='CREADA').first():
+        if not ClaimStatus.query.filter_by(name='CREADA').first():
             rows.append(ClaimStatus(name='CREADA'))
-        # if not ClaimStatus.query.filter_by(ClaimStatus.name == 'PENDIENTE').first():
-        #     rows.append(ClaimStatus(name='PENDIENTE'))
-        #
-        # if not ClaimStatus.query.filter_by(ClaimStatus.name == 'FINALIZADA').first():
-        #     rows.append(ClaimStatus(name='FINALIZADA'))
+
+        if not ClaimStatus.query.filter_by(name='PENDIENTE').first():
+            rows.append(ClaimStatus(name='PENDIENTE'))
+
+        if not ClaimStatus.query.filter_by(name='FINALIZADA').first():
+            rows.append(ClaimStatus(name='FINALIZADA'))
 
         try:
-
             db.session.bulk_save_objects(rows)
             db.session.commit()
         except Exception as e:
@@ -23,17 +23,15 @@ class ClaimInitializer():
 
     def init_type_claims(self):
         rows = []
-        if not ClaimType.query.filter(ClaimType.name.in_('Reclamo')).first():
-            claim_obj = ClaimType()
-            claim_obj.name = 'Reclamo'
-            # rows.append(ClaimType(name='Reclamo'))
+        if not ClaimType.query.filter_by(name='Reclamo').first():
+            rows.append(ClaimType(name='Reclamo'))
 
-        # if not ClaimType.filter(ClaimType.name.in_('General')).first():
-        #     rows.append(ClaimType(name='General'))
+        if not ClaimType.query.filter_by(name='General').first():
+            rows.append(ClaimType(name='General'))
 
         try:
-            # db.session.bulk_save_objects(rows)
-            db.session.add(claim_obj)
+            db.session.bulk_save_objects(rows)
+            # db.session.add(claim_obj)
             db.session.commit()
         except Exception as e:
             db.session.rollback()

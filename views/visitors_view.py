cf5efcd96aa9ede285b14acceb7c02f00a62a81a
SantiInnocenti <santi.innocenti@hotmail.com>
2018-10-23 16:49:41 -0300

    Add post visitor method

--- views/visitors_view.py
+++ views/visitors_view.py
@@ -33,3 +33,31 @@ class VisitorView(FlaskView):
         return jsonify({'Visitantes' : visitantes_data})
 
     def post(self):
+        data = request.json
+        visitor_obj = Visitor()
+
+        visitor_obj.name = data.get('name', None)
+        if not visitor_obj.name:
+            raise BadRequest('Visitor name is Mandatory')
+        visitor_obj.lastname = data.get('lastname', None)
+        if not visitor_obj.lastname:
+            raise BadRequest('Visitor lastname is Mandatory')
+        visitor_obj.dni = data.get('dni', None)
+        if not visitor_obj.dni:
+            raise BadRequest('Visitor Dni is Mandatory')
+        visitor_obj.sex = data.get('sex', None)
+        if not visitor_obj.sex:
+            raise BadRequest('Visitor sex is Mandatory')
+
+        try:
+            db.session.add(visitor_obj)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            print(str(e))
+            raise InternalServerError('Unable to store a new Visitor')
+
+        visitor_save = Visitor.query.order_by(Visitor.id.desc()).first()
+        visitor_data  = self.visitor_schema.dump(visitor_save)
+
+        return jsonify({'Visitor': visitor_data})
